b68788503f5c0905999692d1c8acaa9de43b454a
heyajj <ajanich@ajanich-macbookpro.roam.corp.google.com>
2014-06-19 09:46:47 -0700

    setting up fb post week one lesson

--- FBPost/AppDelegate.m
+++ FBPost/AppDelegate.m
@@ -7,13 +7,17 @@
 //
 
 #import "AppDelegate.h"
+#import "PostViewController.h"
 
 @implementation AppDelegate
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions
 {
     self.window = [[UIWindow alloc] initWithFrame:[[UIScreen mainScreen] bounds]];
-    // Override point for customization after application launch.
+   
+    PostViewController *vc = [[PostViewController alloc] init];
+    self.window.rootViewController = vc;
+    
     self.window.backgroundColor = [UIColor whiteColor];
     [self.window makeKeyAndVisible];
     return YES;
---
+++ PostViewController.h
@@ -0,0 +1,13 @@
+//
+//  PostViewController.h
+//  FBPost
+//
+//  Created by Andrew Janich on 6/19/14.
+//  Copyright (c) 2014 Google. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface PostViewController : UIViewController
+
+@end
